6015569718d52fdfdd7eecb912c85a5c8d48f88a
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-04-24 16:01:18 -0300

    feat: starting main dating screen

--- app/src/main/java/com/example/mobileapp/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -1,16 +1,6 @@
 package com.example.mobileapp;
 
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,9 +9,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private CheckBox harry;
-    private RadioGroup radioGroup;
-    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,45 +19,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        harry = findViewById(R.id.checkboxHarry);
-        progressBar = findViewById(R.id.progressBar);
-        radioGroup = findViewById(R.id.radioGroup);
 
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-                    progressBar.incrementProgressBy(1);
-                    SystemClock.sleep(100);
-                }
-            }
-        });
-
-        thread.start();
-
-        if (harry.isChecked()) {
-            Toast.makeText(MainActivity.this, "Nice, you watche hp", Toast.LENGTH_SHORT).show();
-        }
-        harry.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(MainActivity.this, "Nice, you watche hp", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "You need to watch HP", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.married) {
-                    progressBar.setVisibility(View.GONE);
-                } else {
-                    progressBar.setVisibility(View.VISIBLE);
-                }
-            }
-        });
     }
 }
